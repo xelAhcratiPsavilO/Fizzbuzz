917bd9ce0e341ceac28822d1e7e815f8131e66da
Alex <alejandropitarcholivas@gmail.om>
2018-02-14 14:23:23 +0000

    full fizzbuzz_spec.rb and full fizzbuzz.rb version2

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,6 +1,10 @@
-def fizzbuzz(number)
-  return 'fizzbuzz' if number % 15 == 0
-  return 'fizz' if number % 3 == 0
-  return 'buzz' if number % 5 == 0
-  number
+class Integer
+
+  def fizzbuzz
+    return 'fizzbuzz' if self % 15 == 0
+    return 'fizz' if self % 3 == 0
+    return 'buzz' if self % 5 == 0
+   self
+  end
+
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,13 +1,13 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
+  it 'returns "fizz" for the number 3' do
+    expect(3.fizzbuzz).to eq "fizz"
   end
-  it 'returns "buzz" when passed 5' do
-    expect(fizzbuzz(5)).to eq 'buzz'
+  it 'returns "buzz" for the number 5' do
+    expect(5.fizzbuzz).to eq "buzz"
   end
-  it 'returns "fizzbuzz" when passed 15' do
-    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  it 'returns "fizzbuzz" for the number 15' do
+    expect(15.fizzbuzz).to eq "fizzbuzz"
   end
 end
